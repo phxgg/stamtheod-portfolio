bd2f27223fd90e99974e705f3817af05cc402eee
phxgg <stamatis.1702@gmail.com>
2023-01-29 09:51:06 +0200

    update: some animations in nav links

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -23,7 +23,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="hidden items-center space-x-2 md:flex">
-        <Icons.logo className="h-6 w-6" />
+        <Icons.logo className="h-6 w-6 hover:animate-wave" />
         <span className="hidden font-bold sm:inline-block">
           {siteConfig.name}
         </span>
@@ -38,7 +38,8 @@ export function MainNav({ items }: MainNavProps) {
                   href={item.href}
                   className={cn(
                     "flex items-center text-lg font-semibold text-slate-600 hover:text-slate-900 dark:text-slate-100 sm:text-sm",
-                    item.disabled && "cursor-not-allowed opacity-80"
+                    item.disabled && "cursor-not-allowed opacity-80",
+                    "duration-200 hover:-rotate-6"
                   )}
                 >
                   {item.title}
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -31,7 +31,7 @@ export default function ProjectsPage() {
         </div>
 
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project) => (
+          {projects?.map((project) => (
             <div
               key={project.title}
               className={cn(
