9336679155aa591f8ee2f7b5b9fcd050c561e99e
phxgg <stamatis.1702@gmail.com>
2025-01-04 13:50:04 +0200

    chore: update deps and react to v19

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tailwindcss from 'eslint-plugin-tailwindcss';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const configs = [
+  {
+    ignores: [
+      'dist/*',
+      '**/.cache',
+      '**/public',
+      '**/node_modules',
+      '**/*.esm.js',
+    ],
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:tailwindcss/recommended'
+  ),
+  {
+    plugins: {
+      tailwindcss,
+    },
+
+    settings: {
+      tailwindcss: {
+        callees: ['cn'],
+      },
+    },
+
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+      'react/jsx-key': 'off',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  },
+];
+
+export default configs;
